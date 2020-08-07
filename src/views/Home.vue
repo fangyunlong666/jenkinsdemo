c2cd9aa3b1faf75fa5abb278ccc664185fe96e01
fangyunlong <fangyunlong@qutoutiao.net>
2020-08-07 16:57:15 +0800

    fix: bugs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Jenkins 好的 666"/>
+    <HelloWorld msg="Welcome to Jenkins"/>
   </div>
 </template>
 
