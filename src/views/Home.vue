89be66373b3a40421adc5e7408fcad282c67e9f1
fangyunlong <fangyunlong@qutoutiao.net>
2020-08-07 16:07:46 +0800

    fix: again

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Jenkins, Welcome again"/>
+    <HelloWorld msg="Welcome to Jenkins, Welcome again again"/>
   </div>
 </template>
 
